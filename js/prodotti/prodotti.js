9dc85aa44d59920a4729a55cd97136794ebe8deb
backjello <jackbello97@gmail.com>
2024-10-30 11:00:50 +0100

    30-10

--- js/prodotti/prodotti.js
+++ js/prodotti/prodotti.js
@@ -5,6 +5,43 @@ async function getProdotti() {
             'Authorization': 'Bearer ' + token
         }
     })
+    return result.data
 }
 
-getProdotti()
+async function main() {
+    const data = await getProdotti()
+    const table = document.querySelector('table')
+    for (const prod of data.products) {
+        table.innerHTML += `
+            <tr>
+                <td>
+                    <img src="${prod.images[0]}" style="width:200px">
+                </td>
+                <td>
+                    <a href="/js/singolo-prodotto?id=${prod.id}">
+                    ${prod.title}
+                    </a>
+                    </td>
+                <td>
+                    ${prod.description}
+                </td>
+                <td>
+                    ${prod.price}$
+                </td>
+                <td>
+                    ${prod.stock}
+                </td>
+                <td>
+                    <button onclick="modificaProdotto(${prod.id})">
+                        Modifica
+                    </button>
+                    <button onclick="eliminaProdotto(${prod.id})">
+                        Elimina
+                    </button>
+                </td>
+            </tr>
+        `
+    }
+}
+
+main()
